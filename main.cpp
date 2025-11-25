1f67159cdeac6b979597225c035171fdfc4b6aac
Aida_Yuka <K024G0052@g.neec.ac.jp>
2025-11-25 12:59:20 +0900

    01-01

--- main.cpp
+++ main.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
 #include <Windows.h>
+#include<list>
+#include <iostream>
 
-int main()
+using namespace std;
+
+int main(void)
 {
-	SetConsoleOutputCP(65001);
-	char str[] = "ソラ";
-	printf("%s", str);
+	list<const char*> eki_list
+	{
+		"Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno","Uguisudani", "Nippori", "Tabata", "Komagome","Sugamo", "Otsuka", "Ikebukuro", "Mejiro","Takadanobaba", "Sin-Okubo", "Shinjuku", "Yoyogi","Harajuku", "Shibuya", "Ebisu", "Meguro", "Gotanda","Osaki", "Sinagawa", "Tamachi", "Hamamatsucho", "Shimbashi", "Yurakucho"
+	};
+
+	printf("1970年\n");
+	for (list<const char*>::iterator it_f = eki_list.begin(); it_f != eki_list.end(); it_f++)
+	{
+		std::cout << *it_f << endl;
+	}
+
+	printf("\n2019年\n");
+	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr/*駅名リストを順番にループ*/)
+	{
+		if (strcmp(*itr,"Tabata")==0/*○○駅だったら*/)
+		{
+			itr = eki_list.insert(itr, "Nishi-Nippori");
+			std::cout << *itr << endl;
+			++itr;
+		}
+		std::cout << *itr << endl;
+	}
+
+	printf("\n2022年\n");
+	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr/*駅名リストを順番にループ*/)
+	{
+		if (strcmp(*itr, "Tamachi") == 0/*○○駅だったら*/)
+		{
+			itr = eki_list.insert(itr, "Takanawa Gateway");
+			std::cout << *itr << endl;
+			++itr;
+		}
+		std::cout << *itr << endl;
+	}
+
 	return 0;
 }
